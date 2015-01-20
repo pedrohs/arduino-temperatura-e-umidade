caa19496f5fe88eb008b3f9e461d57ba293cde51
pedrohs <9.pedro.deoliveira@gmail.com>
2015-01-20 13:08:36 -0200

    fix função led

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -5,64 +5,81 @@
 #define DHTPIN A0 //Pino do DHT
 #define DHTTYOE DHT11 //Modelo DHT
 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-byte ip[] = { 192, 168, 1, 110 };
-     
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+
+IPAddress ip(192,168,1,110);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+
 EthernetServer server(80);
 DHT dht(DHTPIN, DHTTYOE);
 
-String readString;
 int Led = 2;
-  
-void setup(){
-  dht.begin();
-  pinMode(Pin, OUTPUT);
-  Ethernet.begin(mac, ip);
-  server.begin();
-}
-  
-void loop(){
-  float temp = dht.readTemperature();//Temperatura
- float umid = dht.readHumidity();//Umidade
 
-  EthernetClient client = server.available();
-  if (client) {
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
+
+void setup()
+{
+  Ethernet.begin(mac,ip,gateway,subnet);
+  server.begin();
   
-        if (readString.length() < 100) {
-          readString += c;              
-        }
+  pinMode(Led, OUTPUT); 
+}
+void loop()
+{
+  EthernetClient client = server.available();
+  if(client)
+  {
+    boolean continua = true;
+    String linha = "";
+
+    while(client.connected())
+    {
+      if(client.available()){
+        char c = client.read();
+        linha.concat(c);
+  
+        if(c == '\n' && continua)
+        {
+          client.println("HTTP/1.1 200 OK");
+          
+          client.println("Content-Type: text/javascript");
+          client.println("Access-Control-Allow-Origin: *");
+          client.println();          
+         
+          int iniciofrente = linha.indexOf("?");
+                
+          if(iniciofrente>-1){
+            iniciofrente     = iniciofrente+6;
+            int fimfrente    = iniciofrente+3;
+            String acao    = linha.substring(iniciofrente,fimfrente);
+            
  
-        if (c == '\n') {
-         client.println("HTTP/1.1 200 OK");
-         client.println("Content-Type: application/json");
-         client.println();
-         client.print("{");
-         client.print("\"temp\":");
-         client.print(temp);
-         client.print(",");
-         client.print("\"umid\":");
-         client.print(umid);
-         client.println("}");
-           
-          delay(1);
-          client.stop();
-           
-          if(readString.indexOf("?ledon") > 0)
-          {
-            digitalWrite(Led, HIGH);
-          }
-          else {
-            if(readString.indexOf("?ledoff") > 0)
-            {
-              digitalWrite(Led, LOW);
-            }
-          }
-          readString="";     
+            if      ( acao == "off"){ digitalWrite(Led, LOW); } 
+            else if ( acao == "oon"){ digitalWrite(Led, HIGH);} 
+            else {}
+            
+            float temp = dht.readTemperature();//Temperatura
+            float umid = dht.readHumidity();//Umidade
+            
+            client.print("{");
+            client.print("\"temp\":");
+            client.print(temp);
+            client.print(",");
+            client.print("\"umid\":");
+            client.print(umid);
+            client.print(",");
+            client.print("\"led\":");
+            client.print(digitalRead(Led));
+            client.println("}");
+            
+         }          
+          break;
         }
+        if(c == '\n') { continua = true; }
+        else if (c != '\r') { continua = false; }
       }
     }
+     delay(1);
+     client.stop();
   }
 }
